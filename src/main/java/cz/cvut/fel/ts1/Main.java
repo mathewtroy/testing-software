006a1ab77f6fa8e5d8a448f0bfd990928a630ede
Vladyslav Hordiienko <hordivla@fel.cvut.cz>
2023-03-19 12:27:45 +0100

    Start

--- src/main/java/cz/cvut/fel/ts1/Main.java
+++ src/main/java/cz/cvut/fel/ts1/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package cz.cvut.fel.ts1;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/test/java/cz/cvut/fel/ts1/MainTest.java
@@ -0,0 +1,10 @@
+package cz.cvut.fel.ts1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
