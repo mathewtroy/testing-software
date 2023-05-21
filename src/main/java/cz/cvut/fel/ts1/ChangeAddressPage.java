5b026ad88156dfb7cb5039c78ebeab214034a796
Vladyslav Hordiienko <hordivla@fel.cvut.cz>
2023-05-21 17:37:41 +0200

    Change address page and test

---
+++ src/main/java/cz/cvut/fel/ts1/ChangeAddressPage.java
@@ -0,0 +1,125 @@
+package cz.cvut.fel.ts1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ChangeAddressPage {
+    private WebDriver driver;
+
+    @FindBy(xpath = "//*[@id='vorname-l']")
+    private WebElement jmenoField;
+
+    @FindBy(xpath = "//*[@id='nachname-l']")
+    private WebElement prijmeniField;
+
+    @FindBy(xpath = "//*[@id='strasse-l']")
+    private WebElement uliceField;
+
+    @FindBy(xpath = "//*[@id='hausnr-l']")
+    private WebElement cisloDomuField;
+
+    @FindBy(xpath = "//*[@id='plz-l']")
+    private WebElement pscField;
+
+    @FindBy(xpath = "//*[@id='ort-l']")
+    private WebElement mestoField;
+
+    @FindBy(xpath = "//*[@id='telefon-l']")
+    private WebElement phoneField;
+
+    @FindBy(xpath = "//*[@id=\"checkOutForm\"]/div[9]/div/input")
+    private WebElement ulozitButt;
+
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[1]/div[1]/div[1]/a")
+    private WebElement changeAddressButt;
+
+
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[2]/div/ul/li[1]/span[2]")
+    private WebElement jmenoText;
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[2]/div/ul/li[1]/span[3]")
+    private WebElement prijmeniText;
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[2]/div/ul/li[2]/span[1]")
+    private WebElement uliceText;
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[2]/div/ul/li[2]/span[2]")
+    private WebElement cisloDomuText;
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[2]/div/ul/li[3]/span[1]")
+    private WebElement pscText;
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[2]/div/ul/li[3]/span[2]")
+    private WebElement mestoText;
+
+    @FindBy(xpath = "//*[@id=\"siteMain\"]/div/div[2]/div[2]/div/ul/li[5]/span")
+    private WebElement phoneText;
+
+
+    public ChangeAddressPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setJmeno(String str){
+        jmenoField.clear();
+        jmenoField.sendKeys(str);
+    }
+
+    public void setPrijmeni(String str){
+        prijmeniField.clear();
+        prijmeniField.sendKeys(str);
+    }
+
+    public void setUlice(String str){
+        uliceField.clear();
+        uliceField.sendKeys(str);
+    }
+
+    public void setPsc(String str){
+        pscField.clear();
+        pscField.sendKeys(str);
+    }
+
+    public void setCisloDomu(String str){
+        cisloDomuField.clear();
+        cisloDomuField.sendKeys(str);
+    }
+
+    public void setMesto(String str){
+        mestoField.clear();
+        mestoField.sendKeys(str);
+    }
+
+    public void setPhone(String str){
+        phoneField.clear();
+        phoneField.sendKeys(str);
+    }
+
+    public void ulozit(){ulozitButt.click();}
+
+    public void clickChangeAddressB(){changeAddressButt.click();}
+
+
+    public String getJmenoText(){return jmenoText.getText();}
+
+    public String getPrijmeniText(){return prijmeniText.getText();}
+
+    public String getUliceText(){return uliceText.getText();}
+
+    public String getPscText(){ return pscText.getText();}
+
+    public String getCisloDomuText(){return cisloDomuText.getText();}
+
+    public String getMestoText(){return mestoText.getText();}
+
+    public String getPhoneText(){return phoneText.getText();}
+
+
+
+
+}
---
+++ src/test/java/cz/cvut/fel/ts1/ChangeAddressPageTest.java
@@ -0,0 +1,72 @@
+package cz.cvut.fel.ts1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChangeAddressPageTest {
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private ChangeAddressPage changeAddressPage;
+    private String loginUrl = "https://www.pinkorblue.cz/Konto/";
+
+
+    @BeforeEach
+    public void setUp() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--whitelisted-ips=\"\"");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        loginPage = new LoginPage(driver);
+        changeAddressPage = new ChangeAddressPage(driver);
+        driver.get(loginUrl);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    public void login() {
+        loginPage.acceptCookie();
+        loginPage.setEmail("qwerty12@cz.cz");
+        loginPage.setPassword("qwerty12@cz.cz");
+        loginPage.clickLogin();
+    }
+    //Here must be CSV
+    @Test
+    public void changeAddress(){
+        login();
+        changeAddressPage.clickChangeAddressB();
+        changeAddressPage.setJmeno("Martin");
+        changeAddressPage.setPrijmeni("Klima");
+        changeAddressPage.setUlice("Dejvicka");
+        changeAddressPage.setCisloDomu("2");
+        changeAddressPage.setPsc("16800");
+        changeAddressPage.setMesto("Brno");
+        changeAddressPage.setPhone("123456789");
+
+        changeAddressPage.ulozit();
+        Assertions.assertEquals("Martin", changeAddressPage.getJmenoText());
+        Assertions.assertEquals("Klima", changeAddressPage.getPrijmeniText());
+        Assertions.assertEquals("Dejvicka", changeAddressPage.getUliceText());
+        Assertions.assertEquals("2", changeAddressPage.getCisloDomuText());
+        Assertions.assertEquals("16800", changeAddressPage.getPscText());
+        Assertions.assertEquals("Brno", changeAddressPage.getMestoText());
+        Assertions.assertEquals("123456789", changeAddressPage.getPhoneText());
+
+    }
+
+//    @AfterEach
+//    public void tearDown() {
+//        driver.quit();
+//    }
+}
